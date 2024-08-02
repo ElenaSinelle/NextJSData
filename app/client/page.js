2b4ab01f71cb8917f2b689609d8e716dfe487595
Elena Sinelnikova <elle.sinelle@gmail.com>
2024-08-02 19:26:55 +0300

    init: init project

---
+++ app/client/page.js
@@ -0,0 +1,9 @@
+import styles from "./page.module.css";
+
+export default function Client() {
+  return (
+    <main className={styles.main}>
+     <h1> It's Client Page</h1>
+    </main>
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,10 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+     <h1> It's My Main</h1>
+
+    </main>
+  );
+}
